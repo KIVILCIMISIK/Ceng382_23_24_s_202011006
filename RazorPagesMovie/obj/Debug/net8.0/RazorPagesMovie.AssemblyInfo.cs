775fa61361a81ed3ccdf60548357692eb058fdb1
KIVILCIMISIK <kvlcmisk@gmail.com>
2024-03-25 00:42:02 +0300

    I created 2 new buttons for the Lab5page. The 1st button controls the hiding and display of the table at the bottom of the page. The 2nd button displays a form that performs addition operations. The opened form calculates the sum of the 2 entered numbers.

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0588a6ded991f407f2b9eeff983fe38de2e539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8864257d03bd0023ecb9a8b4fc42e31436bd98")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
