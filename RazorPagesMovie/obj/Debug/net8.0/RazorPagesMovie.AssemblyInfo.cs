8fd128bcc516d19c485417638ae7c8b914d210ab
Yazgı Kavukçu <65962091+KodMachine@users.noreply.github.com>
2024-06-13 11:49:51 +0300

    not

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775fa61361a81ed3ccdf60548357692eb058fdb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6b32f112e7024501d2972789d6bbae00c92f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
